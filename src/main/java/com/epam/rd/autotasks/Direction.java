9944ec10c6a411fae174b57aa9961700d1a8782d
snowwhitesheep <snw.wht.lk@gmail.com>
2023-05-22 11:11:10 +0200

    ofDegrees

--- src/main/java/com/epam/rd/autotasks/Direction.java
+++ src/main/java/com/epam/rd/autotasks/Direction.java
@@ -3,7 +3,9 @@ package com.epam.rd.autotasks;
 import java.util.Optional;
 
 public enum Direction {
-    N(0), NE(45), E(90), SE(135), S(180), SW(225), W(270), NW(315);
+    N(0), NE(45), E(90),
+    SE(135), S(180), SW(225),
+    W(270), NW(315);
 
     Direction(final int degrees) {
         this.degrees = degrees;
@@ -12,7 +14,36 @@ public enum Direction {
     private int degrees;
 
     public static Direction ofDegrees(int degrees) {
-        throw new UnsupportedOperationException();
+
+        //throw new UnsupportedOperationException();
+        if(degrees > 360){
+            if( (degrees - 360) < 360){
+                degrees -= 360;
+            }else{
+
+            }
+
+        }
+
+        switch (degrees){
+            case 0: Direction n = Direction.N;
+            return n;
+            case 45: Direction ne = Direction.NE;
+            return ne;
+            case 90: Direction e = Direction.E;
+            return e;
+            case 135: Direction se = Direction.SE;
+            return se;
+            case 180: Direction s = Direction.S;
+            return s;
+            case 225: Direction sw = Direction.SW;
+            return sw;
+            case 270: Direction w = Direction.W;
+            return w;
+            case 315: Direction nw = Direction.NW;
+            return nw;
+        }
+        return null;
     }
 
     public static Direction closestToDegrees(int degrees) {
